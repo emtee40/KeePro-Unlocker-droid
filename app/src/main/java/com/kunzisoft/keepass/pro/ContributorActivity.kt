cd19c7e880261a14bbea1cde5f26b30e3b0a49c0
J-Jamet <jeremy.jamet@kunzisoft.com>
2022-03-15 17:41:05 +0100

    Export settings when available in the app

--- app/src/main/java/com/kunzisoft/keepass/pro/ContributorActivity.kt
+++ app/src/main/java/com/kunzisoft/keepass/pro/ContributorActivity.kt
@@ -1,12 +1,16 @@
 package com.kunzisoft.keepass.pro
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.kunzisoft.keepass.pro.databinding.ActivityContributorBinding
+import java.util.*
 
 
 /**
@@ -15,6 +19,7 @@ import com.kunzisoft.keepass.pro.databinding.ActivityContributorBinding
 class ContributorActivity : AppCompatActivity() {
 
     private lateinit var layoutBinding: ActivityContributorBinding
+    private var mExternalFileHelper: ExternalFileHelper? = null
 
     /**
      * Initialize the Google Pay API on creation of the activity
@@ -31,11 +36,37 @@ class ContributorActivity : AppCompatActivity() {
         layoutBinding.keepassdxButton.setOnClickListener {
             openExternalApp("com.kunzisoft.keepass.free")
         }
-        layoutBinding.keepassdxExportSettingsButton.setOnClickListener {
-            // TODO
-        }
-        layoutBinding.keepassdxHowExportSettings.setOnClickListener {
-            gotoUrl("https://github.com/Kunzisoft/KeePassDX/wiki/Import-and-Export#app-properties")
+        if (containsAppProperties(this)) {
+            mExternalFileHelper = ExternalFileHelper(this)
+            mExternalFileHelper?.buildCreateDocument { createdFileUri ->
+                // Export app properties result
+                try {
+                    createdFileUri?.let { uri ->
+                        contentResolver?.openOutputStream(uri)?.use { outputStream ->
+                            getAppProperties(this)
+                                .store(outputStream, getString(R.string.description_app_properties))
+                        }
+                        Toast.makeText(this, R.string.success_export_app_properties, Toast.LENGTH_LONG).show()
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(this, R.string.error_export_app_properties, Toast.LENGTH_LONG).show()
+                    Log.e("Contributor", "Unable to export app properties", e)
+                }
+            }
+
+            layoutBinding.keepassdxExportText.isVisible = true
+            layoutBinding.keepassdxExportSettingsButton.isVisible = true
+            layoutBinding.keepassdxHowExportSettings.isVisible = true
+            layoutBinding.keepassdxExportSettingsButton.setOnClickListener {
+                mExternalFileHelper?.createDocument(getString(R.string.app_properties_file_name))
+            }
+            layoutBinding.keepassdxHowExportSettings.setOnClickListener {
+                gotoUrl("https://github.com/Kunzisoft/KeePassDX/wiki/Import-and-Export#app-properties")
+            }
+        } else {
+            layoutBinding.keepassdxExportText.isVisible = false
+            layoutBinding.keepassdxExportSettingsButton.isVisible = false
+            layoutBinding.keepassdxHowExportSettings.isVisible = false
         }
 
         layoutBinding.filesyncButton.setOnClickListener {
@@ -46,7 +77,7 @@ class ContributorActivity : AppCompatActivity() {
         }
     }
 
-    fun openExternalApp(packageName: String) {
+    private fun openExternalApp(packageName: String) {
         var launchIntent: Intent? = null
         try {
             launchIntent = packageManager.getLaunchIntentForPackage(packageName)?.apply {
@@ -69,7 +100,7 @@ class ContributorActivity : AppCompatActivity() {
         }
     }
 
-    fun gotoUrl(url: String?) {
+    private fun gotoUrl(url: String?) {
         try {
             if (url != null && url.isNotEmpty()) {
                 // Default http:// if no protocol specified
@@ -83,5 +114,99 @@ class ContributorActivity : AppCompatActivity() {
         } catch (e: Exception) {
             Toast.makeText(this, R.string.no_url_handler, Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun containsAppProperties(context: Context): Boolean {
+        var containsKeePassProperties = false
+        for ((name, _) in PreferenceManager.getDefaultSharedPreferences(context).all) {
+            containsKeePassProperties = isAppProperty(name)
+        }
+        return containsKeePassProperties
+    }
+
+    private fun isAppProperty(name: String): Boolean {
+        return when (name) {
+            getString(R.string.allow_no_password_key) -> true
+            getString(R.string.delete_entered_password_key) -> true
+            getString(R.string.enable_read_only_key) -> true
+            getString(R.string.enable_auto_save_database_key) -> true
+            getString(R.string.enable_keep_screen_on_key) -> true
+            getString(R.string.auto_focus_search_key) -> true
+            getString(R.string.subdomain_search_key) -> true
+            getString(R.string.app_timeout_key) -> true
+            getString(R.string.lock_database_screen_off_key) -> true
+            getString(R.string.lock_database_back_root_key) -> true
+            getString(R.string.lock_database_show_button_key) -> true
+            getString(R.string.password_length_key) -> true
+            getString(R.string.list_password_generator_options_key) -> true
+            getString(R.string.hide_password_key) -> true
+            getString(R.string.allow_copy_password_key) -> true
+            getString(R.string.remember_database_locations_key) -> true
+            getString(R.string.show_recent_files_key) -> true
+            getString(R.string.hide_broken_locations_key) -> true
+            getString(R.string.remember_keyfile_locations_key) -> true
+            getString(R.string.biometric_unlock_enable_key) -> true
+            getString(R.string.device_credential_unlock_enable_key) -> true
+            getString(R.string.biometric_auto_open_prompt_key) -> true
+            getString(R.string.temp_advanced_unlock_enable_key) -> true
+            getString(R.string.temp_advanced_unlock_timeout_key) -> true
+
+            getString(R.string.magic_keyboard_key) -> true
+            getString(R.string.clipboard_notifications_key) -> true
+            getString(R.string.clear_clipboard_notification_key) -> true
+            getString(R.string.clipboard_timeout_key) -> true
+            getString(R.string.settings_autofill_enable_key) -> true
+            getString(R.string.keyboard_notification_entry_key) -> true
+            getString(R.string.keyboard_notification_entry_clear_close_key) -> true
+            getString(R.string.keyboard_entry_timeout_key) -> true
+            getString(R.string.keyboard_selection_entry_key) -> true
+            getString(R.string.keyboard_search_share_key) -> true
+            getString(R.string.keyboard_save_search_info_key) -> true
+            getString(R.string.keyboard_auto_go_action_key) -> true
+            getString(R.string.keyboard_key_vibrate_key) -> true
+            getString(R.string.keyboard_key_sound_key) -> true
+            getString(R.string.keyboard_previous_database_credentials_key) -> true
+            getString(R.string.keyboard_previous_fill_in_key) -> true
+            getString(R.string.keyboard_previous_lock_key) -> true
+            getString(R.string.autofill_close_database_key) -> true
+            getString(R.string.autofill_auto_search_key) -> true
+            getString(R.string.autofill_inline_suggestions_key) -> true
+            getString(R.string.autofill_manual_selection_key) -> true
+            getString(R.string.autofill_save_search_info_key) -> true
+            getString(R.string.autofill_ask_to_save_data_key) -> true
+            getString(R.string.autofill_application_id_blocklist_key) -> true
+            getString(R.string.autofill_web_domain_blocklist_key) -> true
+
+            getString(R.string.setting_style_key) -> true
+            getString(R.string.setting_style_brightness_key) -> true
+            getString(R.string.setting_icon_pack_choose_key) -> true
+            getString(R.string.show_entry_colors_key) -> true
+            getString(R.string.list_entries_show_username_key) -> true
+            getString(R.string.list_groups_show_number_entries_key) -> true
+            getString(R.string.show_otp_token_key) -> true
+            getString(R.string.show_uuid_key) -> true
+            getString(R.string.list_size_key) -> true
+            getString(R.string.monospace_font_fields_enable_key) -> true
+            getString(R.string.hide_expired_entries_key) -> true
+            getString(R.string.enable_education_screens_key) -> true
+
+            getString(R.string.sort_node_key) -> true
+            getString(R.string.sort_group_before_key) -> true
+            getString(R.string.sort_ascending_key) -> true
+            getString(R.string.sort_recycle_bin_bottom_key) -> true
+            getString(R.string.allow_copy_password_first_time_key) -> true
+
+            else -> false
+        }
+    }
+
+    private fun getAppProperties(context: Context): Properties {
+        val properties = Properties()
+        for ((name, value) in PreferenceManager.getDefaultSharedPreferences(context).all) {
+            if (isAppProperty(name)) {
+                properties[name] = value.toString()
+            }
+        }
+        return properties
     }
 }
---
+++ app/src/main/java/com/kunzisoft/keepass/pro/ExternalFileHelper.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2019 Jeremy Jamet / Kunzisoft.
+ *
+ * This file is part of KeePassDX.
+ *
+ *  KeePassDX is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  KeePassDX is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KeePassDX.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.kunzisoft.keepass.pro
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+class ExternalFileHelper(context: FragmentActivity) {
+
+    private var activity: FragmentActivity? = context
+
+    private var createDocumentResultLauncher: ActivityResultLauncher<String>? = null
+
+    fun buildCreateDocument(typeString: String = "application/octet-stream",
+                            onFileCreated: (fileCreated: Uri?)->Unit) {
+
+        val resultCallback = ActivityResultCallback<Uri?> { result ->
+            onFileCreated.invoke(result)
+        }
+
+        createDocumentResultLauncher =
+            activity?.registerForActivityResult(
+                CreateDocument(typeString),
+                resultCallback
+            )
+    }
+
+    fun createDocument(titleString: String) {
+        try {
+            createDocumentResultLauncher?.launch(titleString)
+        } catch (e: Exception) {
+            Log.e(TAG, "Unable to create document", e)
+        }
+    }
+
+    class CreateDocument(private val typeString: String) : ActivityResultContracts.CreateDocument() {
+        override fun createIntent(context: Context, input: String): Intent {
+            return super.createIntent(context, input).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = typeString
+            }
+        }
+    }
+
+
+    companion object {
+
+        private const val TAG = "OpenFileHelper"
+
+        @SuppressLint("InlinedApi")
+        fun allowCreateDocumentByStorageAccessFramework(packageManager: PackageManager,
+                                                        typeString: String = "application/octet-stream"): Boolean {
+            return when {
+                // To check if a custom file manager can manage the ACTION_CREATE_DOCUMENT
+                Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT -> {
+                    packageManager.queryIntentActivities(Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+                        addCategory(Intent.CATEGORY_OPENABLE)
+                        type = typeString
+                    }, PackageManager.MATCH_DEFAULT_ONLY).isNotEmpty()
+                }
+                else -> true
+            }
+        }
+    }
+}
